f7ed8762fb7c152c09fcf2a6d82f3134d4f9c2d5
GimpFlamingo <rayjkochenderfer@gmail.com>
2020-04-26 17:00:36 -0400

    Finish generate random values exercise

--- generate_random_numbers/src/main.rs
+++ generate_random_numbers/src/main.rs
@@ -1,10 +1,70 @@
+extern crate rand_distr;
 extern crate rand;
 
-use rand::Rng;
+use rand::{Rng, thread_rng};
+use rand::distributions::{Distribution, Uniform, Standard, Alphanumeric};
+use rand_distr::{Distribution as Distr, Normal, NormalError};
+
+#[derive(Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Distribution<Point> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Point {
+        let (rand_x, rand_y) = rng.gen();
+        Point {
+            x: rand_x,
+            y: rand_y,
+        }
+    }
+}
+
+fn generate_random_password() {
+    let rand_string: String = thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(30)
+        .collect();
+
+    println!("{}", rand_string);
+}
+
+fn generate_random_numbers_of_custom_type() {
+    let mut rng = rand::thread_rng();
+    let rand_tuple = rng.gen::<(i32, bool, f64)>();
+    let rand_point: Point = rng.gen();
+    println!("Random tuple: {:?}", rand_tuple);
+    println!("Random Point: {:?}", rand_point);
+}
+
+// fn generate_random_numbers_in_distribution() -> Result<(), NormalError> {
+//     let mut rng = rand::thread_rng();
+//     let normal = Normal::new(2.0, 3.0)?;
+//     let v = normal.sample(&mut rng);
+//     println!("{} is from a N(2.9) distribution", v);
+//     return Ok(())
+// }
+
+// Generates random numbers within an uniform range
+fn generate_random_numbers_in_range_uniform() {
+    let mut rng = rand::thread_rng();
+    let die = Uniform::from(1..7);
+
+    loop {
+        let throw = die.sample(&mut rng);
+        println!("Roll the die: {}", throw);
+        if throw == 6 {
+            break;
+        }
+    }
+}
 
 // Generates random numbers within a range
 fn generate_random_numbers_in_range() {
-
+    let mut rng = rand::thread_rng();
+    println!("Integer: {}", rng.gen_range(0, 10));
+    println!("Float: {}", rng.gen_range(0.0, 10.0));
 }
 
 // Generates random numbers
@@ -23,4 +83,9 @@ fn generate_random_numbers() {
 
 fn main() {
     generate_random_numbers();
+    generate_random_numbers_in_range();
+    generate_random_numbers_in_range_uniform();
+    generate_random_numbers_of_custom_type();
+    generate_random_password();
+    // generate_random_numbers_in_distribution();
 }
